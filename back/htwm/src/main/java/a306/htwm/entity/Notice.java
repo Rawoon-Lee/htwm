93cab435ff4db684e5b6d3d52992593f75227f62
최희선 <asdfmelody@naver.com>
2022-10-21 11:41:35 +0900

    fix: Recreate notice table - #1

--- back/htwm/src/main/java/a306/htwm/entity/Notice.java
+++ back/htwm/src/main/java/a306/htwm/entity/Notice.java
@@ -5,8 +5,8 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.Date;
 
-//@Entity
-//@Table(name = "notice")
+@Entity
+@Table(name = "notice")
 @Getter
 @Setter
 @Builder
@@ -19,7 +19,6 @@ public class Notice {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /*
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "from_id")
     private User fromId;
@@ -27,9 +26,10 @@ public class Notice {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "to_id")
     private User toId;
-    */
 
     private int status;
 
     private Date createTime;
+
+    private boolean isread;
 }
