bb0be1933bc32f903657f7ef40de312c4d8c211c
김보경 <rawoon@kakao.com>
2022-10-25 00:27:11 +0900

    [FE] feat | button for login

--- front_app/HTM/screens/MainPage/Main.tsx
+++ front_app/HTM/screens/MainPage/Main.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 import { TextInput } from "react-native"
+import PrimaryButton from "../../components/PrimaryButton"
 
 function Main() {
 	return (
 		<View style={styles.container}>
 			<Text>여기는 메인화면 입니다</Text>
+			<PrimaryButton>Google 로그인</PrimaryButton>
 		</View>
 	)
 }
