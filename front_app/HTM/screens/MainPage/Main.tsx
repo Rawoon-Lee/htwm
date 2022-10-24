3c4caccb1393e77217ac601b56044ed19cf0cd53
김보경 <rawoon@kakao.com>
2022-10-24 15:50:32 +0900

    [FE] feat |  add react-navigation

---
+++ front_app/HTM/screens/MainPage/Main.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+import { TextInput } from "react-native"
+
+function Main() {
+	return (
+		<View>
+			<Text>Main</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
+}
+
+export default Main
