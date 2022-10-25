30cb09f5c7bc18c0f34c5b4860f598d386114f8a
김보경 <rawoon@kakao.com>
2022-10-25 15:50:03 +0900

    [FE] feat | routine list setup

--- front_app/HTM/screens/RoutinesList/RoutinesList.tsx
+++ front_app/HTM/screens/RoutinesList/RoutinesList.tsx
@@ -1,8 +1,13 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 import { TextInput } from "react-native"
+import { routine } from "../../api/routine"
+import * as React from "react"
 
 function RoutineList() {
+	const [routineList, setRoutineList] = React.useState()
+	// React.useEffect({}, [])
+
 	return (
 		<View>
 			<Text>루틴 리스트</Text>
