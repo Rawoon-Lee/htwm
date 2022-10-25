6570d500e4da0b682cd14693bd56f6772daa7c82
최희선 <asdfmelody@naver.com>
2022-10-25 17:09:35 +0900

    feat: Add get username by uuid

--- back/htwm/src/main/java/a306/htwm/repository/MirrorRepository.java
+++ back/htwm/src/main/java/a306/htwm/repository/MirrorRepository.java
@@ -3,12 +3,18 @@ package a306.htwm.repository;
 import a306.htwm.entity.Mirror;
 import a306.htwm.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MirrorRepository extends JpaRepository<Mirror, Long> {
 
-    Mirror findByUuid(String uuid);
+    @Query(nativeQuery = true, value = "select * from mirror " +
+            "where uuid = :uuid")
+    Mirror findByUuid(@Param("uuid") String uuid);
 
-    Mirror findByUser(User user);
+    @Query(nativeQuery = true, value = "select * from mirror " +
+            "where user_id = :userId")
+    Mirror findByUser(@Param("userId") Long userId);
 }
