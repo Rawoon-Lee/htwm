4aa6bc32a95b7c544af712580529302e518588ed
최희선 <asdfmelody@naver.com>
2022-10-27 15:03:20 +0900

    fix: Change swagger version 3 & Fix socket

--- back/htwm/src/main/java/a306/htwm/config/SwaggerConfig.java
+++ back/htwm/src/main/java/a306/htwm/config/SwaggerConfig.java
@@ -10,12 +10,18 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        Server serverLocal = new Server("local", "http://localhost:8399/api", "for local usages", Collections.emptyList(), Collections.emptyList());
+        Server server = new Server("server", "https://k7a306.p.ssafy.io", "for server", Collections.emptyList(), Collections.emptyList());
+
+        return new Docket(DocumentationType.OAS_30)
+                .servers(serverLocal, server)
                 .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("a306.htwm"))
