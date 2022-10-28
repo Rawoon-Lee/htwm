3cbc6b159b4f6d09a861edce84387b018ecaa217
kd8317 <kd8317@naver.com>
2022-10-28 13:46:03 +0900

    [FE] fix: Fix exe file

--- front_web/src/store/constants.js
+++ front_web/src/store/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // UUID: '0cf2ef96-a64c-4ed3-b093-917d1b5150c7',
-  UUID: 'be2af006-d226-4527-a258-3cce8fedba06',
+  UUID: '0cf2ef96-a64c-4ed3-b093-917d1b5150c7',
+  // UUID: 'be2af006-d226-4527-a258-3cce8fedba06',
   SEND_TEST: 'send_test',
   START_COUNTS: 'start_counts', // 운동 루틴 시작 할 때 web -> electron. 내용은 시작하는 운동 내용
   SEND_COUNTS: 'send_counts', // 운동 카운트시 electron -> web. 내용은 카운트 수
