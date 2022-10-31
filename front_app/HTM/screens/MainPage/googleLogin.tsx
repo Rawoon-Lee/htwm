ecfe8efea1c9c690da8aee5305fd2c2d35b680de
김보경 <rawoon@kakao.com>
2022-10-31 12:32:38 +0900

    [FE] bug | solve google login error

--- front_app/HTM/screens/MainPage/googleLogin.tsx
+++ front_app/HTM/screens/MainPage/googleLogin.tsx
@@ -22,12 +22,12 @@ function GoogleLogin() {
 	const [accessToken, setAccessToken] = React.useState<any | null>(null)
 
 	const [request, response, promptAsync] = Google.useAuthRequest({
-		expoClientId:
-			"440495779704-ekaoouogu6bnahpkh5qka267linn8f2d.apps.googleusercontent.com",
 		androidClientId:
 			"440495779704-3367tl8q0m2rctutebc91ksvbt6t0dho.apps.googleusercontent.com",
 		iosClientId:
-			"440495779704-5uftm1ea7girg4j5v78cbdrjq2lcuoe7.apps.googleusercontent.com"
+			"440495779704-5uftm1ea7girg4j5v78cbdrjq2lcuoe7.apps.googleusercontent.com",
+		expoClientId:
+			"440495779704-ekaoouogu6bnahpkh5qka267linn8f2d.apps.googleusercontent.com"
 	})
 
 	async function storeUserData() {
