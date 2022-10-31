ca1fd9daa886b065ba4ba0423105ef6b8d9d1483
kd8317 <kd8317@naver.com>
2022-10-31 17:59:53 +0900

    [FE] feat: Add routine progressRate

--- front_web/src/pages/Routine/routineList.js
+++ front_web/src/pages/Routine/routineList.js
@@ -1,9 +1,10 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { routine } from '../../actions/api/api'
-import { setRoutineList } from '../../store/modules/util'
+import { setRoutineDetail, setRoutineList } from '../../store/modules/util'
 
-export default function RoutineList() {
+export default function RoutineList(props) {
+  const setState = props.setState
   const dispatch = useDispatch()
   const username = useSelector((state) => state.user.username)
   const routineList = useSelector((state) => state.util.routineList)
@@ -15,18 +16,40 @@ export default function RoutineList() {
     })
   }, [])
 
+  const selectRoutine = (idx) => {
+    let routineDetail = {}
+    routineDetail.name = routineList[idx].name
+    routineDetail.username = routineList[idx].username
+    routineDetail.sets = []
+
+    routineList[idx].sets.map((set) => {
+      if (set.set_cnt !== 0) {
+        for (let i = 0; i < set.set_cnt; i++) {
+          routineDetail.sets.push(set)
+        }
+      } else {
+        routineDetail.sets.push(set)
+      }
+    })
+    console.log(routineDetail)
+    dispatch(setRoutineDetail(routineDetail))
+    setState(1)
+  }
+
   return (
     <div>
-      {routineList.map((routine, idx) => (
-        <div key={routine.name}>
-          {idx + 1}번: {routine.name}
-          <div>
-            {routine.set.map((set) => (
-              <div>{set.exercise_name}</div>
-            ))}
+      {routineList?.length &&
+        routineList.map((routine, idx) => (
+          <div key={routine.name} onClick={() => selectRoutine(idx)}>
+            <div>
+              {idx + 1}번: {routine.name}
+              <div>
+                {routine.sets?.length && routine.sets.map((set, idx) => <span key={idx}>{set.exercise_name} </span>)}
+              </div>
+            </div>
+            <br />
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   )
 }
