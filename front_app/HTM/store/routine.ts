36f84b1c17f00e444bf7a9c2b8393b1f2c788d4f
junsu <xofkdqkqh@naver.com>
2022-11-01 16:17:58 +0900

    FE feat | routine page init

--- front_app/HTM/store/routine.ts
+++ front_app/HTM/store/routine.ts
@@ -1,52 +1,55 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
-export interface RoutineData {
-	color: string
-	name: string
-	username: string
-	sets: SetData[]
+export type RoutineDataList = RoutineData[]
+interface RoutineData {
+    color: string
+    name: string
+    username: string
+    sets: SetData[]
 }
 interface SetData {
-	exercise_id: number
-	exercise_name: string
-	number: number
-	sec: number
-	set_cnt: number
+    exercise_id: number
+    exercise_name: string
+    number: number
+    sec: number
+    set_cnt: number
 }
-const initialStateRoutineData: RoutineData = {
-	color: "#E0BB95",
-	name: "",
-	username: "",
-	sets: [
-		{
-			exercise_id: 0,
-			exercise_name: "스쿼트",
-			number: 0,
-			sec: 0,
-			set_cnt: 0
-		}
-	]
-}
+const initialStateRoutineData: RoutineDataList = [
+    {
+        color: "#E0BB95",
+        name: "",
+        username: "",
+        sets: [
+            {
+                exercise_id: 0,
+                exercise_name: "스쿼트",
+                number: 0,
+                sec: 0,
+                set_cnt: 0
+            }
+        ]
+    }
+]
 const RoutineListSlice = createSlice({
-	name: "userInfo",
-	initialState: initialStateRoutineData,
-	reducers: {
-		getRoutineList: (state, action: PayloadAction<RoutineData>) => {
-			return action.payload
-		}
-	}
+    name: "userInfo",
+    initialState: initialStateRoutineData,
+    reducers: {
+        getRoutineList: (state, action: PayloadAction<RoutineDataList>) => {
+            return action.payload
+        }
+    }
 })
 
 // const userIdSlice = createSlice({
-// 	name: "userId",
-// 	initialState: {
-// 		id: ""
-// 	},
-// 	reducers: {
-// 		getUserId: (state, action: PayloadAction<string>) => {
-// 			state.id = action.payload
-// 		}
-// 	}
+//     name: "userId",
+//     initialState: {
+//         id: ""
+//     },
+//     reducers: {
+//         getUserId: (state, action: PayloadAction<string>) => {
+//             state.id = action.payload
+//         }
+//     }
 // })
 
 export const { getRoutineList } = RoutineListSlice.actions
