23251f810868a06c3189e33cf32563c60715398f
최희선 <asdfmelody@naver.com>
2022-11-15 10:37:03 +0900

    feat: test without sockjs

--- back/htwm/src/main/java/a306/htwm/config/WebSocketConfig.java
+++ back/htwm/src/main/java/a306/htwm/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .addEndpoint("/socket")
                 .setAllowedOriginPatterns("*")
                 .setHandshakeHandler(new CustomHandshakeHandler())
-                .withSockJS()
+//                .withSockJS()
         ;
     }
 
