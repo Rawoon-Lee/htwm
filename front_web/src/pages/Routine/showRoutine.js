3171fa597498e73ee36322e3eec852f07b3fb6a4
김도현 <kd8317@gmail.com>
2022-11-19 23:13:14 +0900

    [FE] feat: Show next routine image in rest time

--- front_web/src/pages/Routine/showRoutine.js
+++ front_web/src/pages/Routine/showRoutine.js
@@ -11,44 +11,40 @@ export default function ShowRoutine(props) {
   useEffect(() => {
     setTimeout(() => {
       setRoutineState(2)
-    }, 16000)
+    }, 1000)
   }, [])
 
-  // useEffect(() => {
-  //   const socket = connect({
-  //     port: 2121,
-  //     // host: '70.12.246.21', //ssafy1102
-  //     // host: '70.12.229.98', //guest
-  //     // host: '192.168.159.137', //phone
-  //     host: '192.168.159.45'
-  //   })
+  useEffect(() => {
+    const socket = connect({
+      port: 2121,
+      // host: '70.12.246.21', //ssafy1102
+      // host: '70.12.229.98', //guest
+      // host: '192.168.159.137', //phone
+      host: '192.168.159.45',
+    })
 
-  //   // setting encoding
-  //   socket.setEncoding('utf8')
+    socket.setEncoding('utf8')
 
-  //   socket.on('connect', function () {
-  //     // send message to server
-  //     socket.write(
-  //       JSON.stringify({
-  //         current_id: '1',
-  //         flag: 'end',
-  //         next_id: `${routineDetail.sets[1].exercise_id}`,
-  //       }),
-  //     )
-  //   })
+    socket.on('connect', function () {
+      socket.write(
+        JSON.stringify({
+          current_id: '1',
+          flag: 'end',
+          next_id: `${routineDetail.sets[1].exercise_id}`,
+        }),
+      )
+    })
+    socket.on('close', function () {
+      console.log('close : show')
+    })
+    socket.on('error', function (err) {
+      console.log('on error: ', err.code)
+    })
 
-  //   socket.on('close', function () {
-  //     console.log('close : show')
-  //   })
-
-  //   socket.on('error', function (err) {
-  //     console.log('on error: ', err.code)
-  //   })
-
-  //   return () => {
-  //     socket.destroy()
-  //   }
-  // }, [])
+    return () => {
+      socket.destroy()
+    }
+  }, [])
 
   return (
     <div className="show-routine">
